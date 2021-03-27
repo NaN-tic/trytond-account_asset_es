ebb067b50c66882e75b5c97103ba75cf9f4cba39
Albert Cervera i Areny <albert@nan-tic.com>
2021-03-27 23:49:56 +0100

    Pyflakes fixes.

--- account.py
+++ account.py
@@ -1,9 +1,7 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import PoolMeta
-from trytond.pyson import If, Eval, Bool
-
-__all__ = ['AccountTypeTemplate', 'AccountType']
+from trytond.pyson import If, Eval
 
 
 class AccountTypeTemplate(metaclass=PoolMeta):
