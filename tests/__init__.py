e69aa5eec0102d8d9db1a72bc4c301f59ce7a920
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:08:42 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-try:
-    from trytond.modules.account_asset_es.tests.test_account_asset_es import suite  # noqa: E501
-except ImportError:
-    from .test_account_asset_es import suite
-
-__all__ = ['suite']
